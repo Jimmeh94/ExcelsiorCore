424d8fdc6d9e18a0ba5075f8ee8626a92686fcbc
Jimmy <jimmy.walters94@yahoo.com>
2018-09-16 16:03:51 -0500

    Update

--- src/main/java/com/excelsiormc/excelsiorcore/ExcelsiorCore.java
+++ src/main/java/com/excelsiormc/excelsiorcore/ExcelsiorCore.java
@@ -14,14 +14,14 @@ import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 
-/*@Plugin(
+@Plugin(
         id = "excelsiorcore",
         name = "ExcelsiorCore",
         description = "A Sponge based API with basic utilities.",
         authors = {
                 "Jimmy"
         }
-)*/
+)
 public class ExcelsiorCore {
 
     public static ExcelsiorCore INSTANCE;
